a21eb094579004ca0acf95b68f21163a7f152ba8
Spencer Nicol <spencer.nicol@healthcatalyst.com>
2020-02-06 15:38:53 -0700

    update xml to json conversion to fix types for boolean and integer

--- Installer/Install-DosReports.ps1
+++ Installer/Install-DosReports.ps1
@@ -7,7 +7,7 @@ if ($modules) { Import-Module $modules -Force -Global }
   <date xmlns:json="http://james.newtonking.com/projects/json">2019-01-01</date>
   <title xmlns:json="http://james.newtonking.com/projects/json">Sepsis Hand Hygiene Report</title>
   <subtitle xmlns:json="http://james.newtonking.com/projects/json">with a subtitle</subtitle>
-  <latest xmlns:json="http://james.newtonking.com/projects/json">true</latest>
+  <latest xmlns:json="http://james.newtonking.com/projects/json" Boolean="true" />
   <group xmlns:json="http://james.newtonking.com/projects/json">
     <domain>Clinical</domain>
     <area>Sepsis</area>
@@ -29,55 +29,55 @@ if ($modules) { Import-Module $modules -Force -Global }
           <label>My First dataset</label>
           <backgroundColor>red</backgroundColor>
           <borderColor>red</borderColor>
-          <data xmlns:json="http://james.newtonking.com/projects/json">10</data>
-          <data xmlns:json="http://james.newtonking.com/projects/json">60</data>
-          <data xmlns:json="http://james.newtonking.com/projects/json">15</data>
-          <data xmlns:json="http://james.newtonking.com/projects/json">5</data>
-          <data xmlns:json="http://james.newtonking.com/projects/json">30</data>
-          <data xmlns:json="http://james.newtonking.com/projects/json">38</data>
-          <data xmlns:json="http://james.newtonking.com/projects/json">60</data>
-          <fill>false</fill>
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="10" />
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="60" />
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="15" />
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="5" />
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="30" />
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="38" />
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="60" />
+          <fill Boolean="false" />
         </datasets>
         <datasets xmlns:json="http://james.newtonking.com/projects/json">
           <label>My Second dataset</label>
           <backgroundColor>blue</backgroundColor>
           <borderColor>blue</borderColor>
-          <data xmlns:json="http://james.newtonking.com/projects/json">5</data>
-          <data xmlns:json="http://james.newtonking.com/projects/json">12</data>
-          <data xmlns:json="http://james.newtonking.com/projects/json">90</data>
-          <data xmlns:json="http://james.newtonking.com/projects/json">65</data>
-          <data xmlns:json="http://james.newtonking.com/projects/json">20</data>
-          <data xmlns:json="http://james.newtonking.com/projects/json">56</data>
-          <data xmlns:json="http://james.newtonking.com/projects/json">12</data>
-          <fill>false</fill>
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="5" />
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="12" />
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="90" />
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="65" />
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="20" />
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="56" />
+          <data xmlns:json="http://james.newtonking.com/projects/json" Integer="12" />
+          <fill Boolean="false" />
         </datasets>
       </data>
       <options xmlns:json="http://james.newtonking.com/projects/json">
-        <responsive>true</responsive>
+        <responsive Boolean="true" />
         <title xmlns:json="http://james.newtonking.com/projects/json">
-          <display>true</display>
+          <display Boolean="true" />
           <text>Chart.js Line Chart</text>
         </title>
         <tooltips xmlns:json="http://james.newtonking.com/projects/json">
           <mode>index</mode>
-          <intersect>false</intersect>
+          <intersect Boolean="false" />
         </tooltips>
         <hover xmlns:json="http://james.newtonking.com/projects/json">
           <mode>nearest</mode>
-          <intersect>true</intersect>
+          <intersect Boolean="true" />
         </hover>
         <scales xmlns:json="http://james.newtonking.com/projects/json">
           <xAxes xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
-            <display>true</display>
+            <display Boolean="true" />
             <scaleLabel>
-              <display xmlns:json="http://james.newtonking.com/projects/json">true</display>
+              <display xmlns:json="http://james.newtonking.com/projects/json" Boolean="true" />
               <labelString xmlns:json="http://james.newtonking.com/projects/json">Month</labelString>
             </scaleLabel>
           </xAxes>
           <yAxes xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
-            <display>true</display>
+            <display Boolean="true" />
             <scaleLabel>
-              <display xmlns:json="http://james.newtonking.com/projects/json">true</display>
+              <display xmlns:json="http://james.newtonking.com/projects/json" Boolean="true" />
               <labelString xmlns:json="http://james.newtonking.com/projects/json">Value</labelString>
             </scaleLabel>
           </yAxes>
@@ -94,8 +94,6 @@ if ($modules) { Import-Module $modules -Force -Global }
 
 ConvertFrom-XmlToJson $xml -OmitRootObject
 
-
-
 <#
 WITH XMLNAMESPACES ('http://james.newtonking.com/projects/json' as [json])
 SELECT (
@@ -105,7 +103,7 @@ SELECT
       '2019-01-01' as [date],
       'Sepsis Hand Hygiene Report' as [title],
       'with a subtitle' as [subtitle],
-      'true' as [latest],
+      'true' as [latest/@Boolean],
       (
         SELECT
           'Clinical' as [domain],
@@ -137,11 +135,11 @@ SELECT
                     'red' as [backgroundColor],
                     'red' as [borderColor],
                     (
-                      SELECT [data]
-                      FROM (VALUES (10), (60), (15), (5), (30), (38), (60)) as d ([data])
+                      SELECT [data/@Integer]
+                      FROM (VALUES (10), (60), (15), (5), (30), (38), (60)) as d ([data/@Integer])
                       FOR XML PATH (''), type
                     ),
-                    'false' as [fill]
+                    'false' as [fill/@Boolean]
                   FOR XML PATH ('datasets'), type
                 ),
                 (
@@ -150,66 +148,66 @@ SELECT
                     'blue' as [backgroundColor],
                     'blue' as [borderColor],
                     (
-                      SELECT [data]
-                      FROM (VALUES (5), (12), (90), (65), (20), (56), (12)) as d ([data])
+                      SELECT [data/@Integer]
+                      FROM (VALUES (5), (12), (90), (65), (20), (56), (12)) as d ([data/@Integer])
                       FOR XML PATH (''), type
                     ),
-                    'false' as [fill]
+                    'false' as [fill/@Boolean]
                   FOR XML PATH ('datasets'), type
                 )
               FOR XML PATH ('data'), type
             ),
             (
               SELECT
-                'true' as [responsive],
+                'true' as [responsive/@Boolean],
                 (
                   SELECT
-                    'true' as [display],
+                    'true' as [display/@Boolean],
                     'Chart.js Line Chart' as [text]
                   FOR XML PATH ('title'), type
                 ),
                 (
                   SELECT
                     'index' as [mode],
-                    'false' as [intersect]
+                    'false' as [intersect/@Boolean]
                   FOR XML PATH ('tooltips'), type
                 ),
                 (
                   SELECT
                     'nearest' as [mode],
-                    'true' as [intersect]
+                    'true' as [intersect/@Boolean]
                   FOR XML PATH ('hover'), type
                 ),
                 (
                   SELECT
                     (
-                      SELECT 'true' as [@json:Array], [display], [scaleLabel]
+                      SELECT 'true' as [@json:Array], [display/@Boolean], [scaleLabel]
                       FROM (VALUES
                         (
                           'true',
                            (
                              SELECT
-                               'true' as [display],
+                               'true' as [display/@Boolean],
                                'Month' as [labelString]
                              FOR XML PATH (''), type
                            )
                         )
-                      ) as l ([display], [scaleLabel])
+                      ) as l ([display/@Boolean], [scaleLabel])
                       FOR XML PATH ('xAxes'), type
                     ),
                     (
-                      SELECT 'true' as [@json:Array], [display], [scaleLabel]
+                      SELECT 'true' as [@json:Array], [display/@Boolean], [scaleLabel]
                       FROM (VALUES
                         (
                           'true',
                            (
                              SELECT
-                               'true' as [display],
+                               'true' as [display/@Boolean],
                                'Value' as [labelString]
                              FOR XML PATH (''), type
                            )
                         )
-                      ) as l ([display], [scaleLabel])
+                      ) as l ([display/@Boolean], [scaleLabel])
                       FOR XML PATH ('yAxes'), type
                     )
                   FOR XML PATH ('scales'), type
