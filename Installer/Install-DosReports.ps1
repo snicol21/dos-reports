6c9b2fe0064697ec96fb589a6c7162e5eec7dcb8
Spencer Nicol <spencer.nicol@healthcatalyst.com>
2020-02-10 13:53:58 -0700

    update the sql scripts to follow a more scalable xml syntax

--- Installer/Install-DosReports.ps1
+++ Installer/Install-DosReports.ps1
@@ -4,75 +4,106 @@ if ($modules) { Import-Module $modules -Force -Global }
 
 [xml]$xml = @'
 <root xmlns:json="http://james.newtonking.com/projects/json">
-  <date xmlns:json="http://james.newtonking.com/projects/json">2019-02-01</date>
-  <title xmlns:json="http://james.newtonking.com/projects/json">Sepsis Rate Report</title>
-  <subtitle xmlns:json="http://james.newtonking.com/projects/json">with a subtitle</subtitle>
-  <latest xmlns:json="http://james.newtonking.com/projects/json" Boolean="true" />
-  <group xmlns:json="http://james.newtonking.com/projects/json">
+  <header xmlns:json="http://james.newtonking.com/projects/json">
     <domain>Clinical</domain>
     <area>Sepsis</area>
-    <report>Rate</report>
-  </group>
+    <report>Hand Hygiene</report>
+    <title>Sepsis Hand Hygiene Report</title>
+    <subtitle>with a subtitle</subtitle>
+    <date>2019-01-01</date>
+    <latest Boolean="true" />
+  </header>
   <sections xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
-    <name>Rate Over Time</name>
-    <chart>
-      <type xmlns:json="http://james.newtonking.com/projects/json">line</type>
-      <data xmlns:json="http://james.newtonking.com/projects/json">
-        <labels xmlns:json="http://james.newtonking.com/projects/json">January</labels>
-        <labels xmlns:json="http://james.newtonking.com/projects/json">February</labels>
-        <labels xmlns:json="http://james.newtonking.com/projects/json">March</labels>
-        <labels xmlns:json="http://james.newtonking.com/projects/json">April</labels>
-        <labels xmlns:json="http://james.newtonking.com/projects/json">May</labels>
-        <labels xmlns:json="http://james.newtonking.com/projects/json">June</labels>
-        <labels xmlns:json="http://james.newtonking.com/projects/json">July</labels>
-        <datasets xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
-          <label>My First dataset</label>
-          <backgroundColor>red</backgroundColor>
-          <borderColor>red</borderColor>
-          <data xmlns:json="http://james.newtonking.com/projects/json" Float="1.00" />
-          <data xmlns:json="http://james.newtonking.com/projects/json" Float="0.78" />
-          <data xmlns:json="http://james.newtonking.com/projects/json" Float="0.68" />
-          <data xmlns:json="http://james.newtonking.com/projects/json" Float="0.70" />
-          <data xmlns:json="http://james.newtonking.com/projects/json" Float="0.72" />
-          <data xmlns:json="http://james.newtonking.com/projects/json" Float="0.65" />
-          <data xmlns:json="http://james.newtonking.com/projects/json" Float="0.62" />
-          <fill Boolean="false" />
-        </datasets>
-      </data>
-      <options xmlns:json="http://james.newtonking.com/projects/json">
-        <responsive Boolean="true" />
-        <title xmlns:json="http://james.newtonking.com/projects/json">
-          <display Boolean="true" />
-          <text>Chart.js Line Chart</text>
-        </title>
-        <tooltips xmlns:json="http://james.newtonking.com/projects/json">
-          <mode>index</mode>
-          <intersect Boolean="false" />
-        </tooltips>
-        <hover xmlns:json="http://james.newtonking.com/projects/json">
-          <mode>nearest</mode>
-          <intersect Boolean="true" />
-        </hover>
-        <scales xmlns:json="http://james.newtonking.com/projects/json">
-          <xAxes xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
-            <display Boolean="true" />
-            <scaleLabel>
-              <display xmlns:json="http://james.newtonking.com/projects/json" Boolean="true" />
-              <labelString xmlns:json="http://james.newtonking.com/projects/json">Month</labelString>
-            </scaleLabel>
-          </xAxes>
-          <yAxes xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
-            <display Boolean="true" />
-            <scaleLabel>
-              <display xmlns:json="http://james.newtonking.com/projects/json" Boolean="true" />
-              <labelString xmlns:json="http://james.newtonking.com/projects/json">Value</labelString>
-            </scaleLabel>
-          </yAxes>
-        </scales>
-      </options>
-    </chart>
+    <markdown xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
+      <markdown>## Section 1</markdown>
+    </markdown>
+    <markdown xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
+      <markdown> * List Item 1</markdown>
+    </markdown>
+    <markdown xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
+      <markdown> * List Item 2</markdown>
+    </markdown>
+    <figures xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
+      <figures>
+        <type xmlns:json="http://james.newtonking.com/projects/json">line-chart</type>
+        <config xmlns:json="http://james.newtonking.com/projects/json">
+          <type xmlns:json="http://james.newtonking.com/projects/json">line</type>
+          <data xmlns:json="http://james.newtonking.com/projects/json">
+            <labels xmlns:json="http://james.newtonking.com/projects/json">January</labels>
+            <labels xmlns:json="http://james.newtonking.com/projects/json">February</labels>
+            <labels xmlns:json="http://james.newtonking.com/projects/json">March</labels>
+            <labels xmlns:json="http://james.newtonking.com/projects/json">April</labels>
+            <labels xmlns:json="http://james.newtonking.com/projects/json">May</labels>
+            <labels xmlns:json="http://james.newtonking.com/projects/json">June</labels>
+            <labels xmlns:json="http://james.newtonking.com/projects/json">July</labels>
+            <datasets xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
+              <label>My First dataset</label>
+              <backgroundColor>red</backgroundColor>
+              <borderColor>red</borderColor>
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="10" />
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="60" />
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="15" />
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="5" />
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="30" />
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="38" />
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="60" />
+              <fill Boolean="false" />
+            </datasets>
+            <datasets xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
+              <label>My Second dataset</label>
+              <backgroundColor>blue</backgroundColor>
+              <borderColor>blue</borderColor>
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="5" />
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="12" />
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="90" />
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="65" />
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="20" />
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="56" />
+              <data xmlns:json="http://james.newtonking.com/projects/json" Integer="12" />
+              <fill Boolean="false" />
+            </datasets>
+          </data>
+          <options xmlns:json="http://james.newtonking.com/projects/json">
+            <responsive Boolean="true" />
+            <title xmlns:json="http://james.newtonking.com/projects/json">
+              <display Boolean="true" />
+              <text>Chart.js Line Chart</text>
+            </title>
+            <tooltips xmlns:json="http://james.newtonking.com/projects/json">
+              <mode>index</mode>
+              <intersect Boolean="false" />
+            </tooltips>
+            <hover xmlns:json="http://james.newtonking.com/projects/json">
+              <mode>nearest</mode>
+              <intersect Boolean="true" />
+            </hover>
+            <scales xmlns:json="http://james.newtonking.com/projects/json">
+              <xAxes xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
+                <display Boolean="true" />
+                <scaleLabel>
+                  <display xmlns:json="http://james.newtonking.com/projects/json" Boolean="true" />
+                  <labelString xmlns:json="http://james.newtonking.com/projects/json">Month</labelString>
+                </scaleLabel>
+              </xAxes>
+              <yAxes xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
+                <display Boolean="true" />
+                <scaleLabel>
+                  <display xmlns:json="http://james.newtonking.com/projects/json" Boolean="true" />
+                  <labelString xmlns:json="http://james.newtonking.com/projects/json">Value</labelString>
+                </scaleLabel>
+              </yAxes>
+            </scales>
+          </options>
+        </config>
+      </figures>
+    </figures>
   </sections>
+  <footer xmlns:json="http://james.newtonking.com/projects/json">
+    <markdown xmlns:json="http://james.newtonking.com/projects/json" json:Array="true">
+      <markdown>© 2020 Health Catalyst. All Rights Reserved.</markdown>
+    </markdown>
+  </footer>
 </root>
 '@
 
-ConvertFrom-XmlToJson $xml -OmitRootObject
+JsonConvert-SerializeXmlNode $xml -OmitRootObject
--- Installer/Scripts/Example_LineChart.sql
+++ Installer/Scripts/Example_LineChart.sql
@@ -3,131 +3,149 @@ SELECT (
 SELECT
   (
     SELECT
-      'Clinical' as [domain],
-      'Sepsis' as [area],
-      'Hand Hygiene' as [report],
+      'Examples' as [domain],
+      'Templates' as [area],
+      'Line Charts' as [report],
+      'Line Charts' as [title],
+      'Example report using line charts' as [subtitle],
+      '2020-02-10' as [date],
       'true' as [latest/@Boolean]
-    FOR XML PATH ('frontmatter'), type
-  ),
-  (
-    SELECT
-      '2019-01-01' as [date],
-      'Sepsis Hand Hygiene Report' as [title],
-      'with a subtitle' as [subtitle]
     FOR XML PATH ('header'), type
   ),
   (
-    SELECT 'true' as [@json:Array], *
-    FROM (VALUES
-      /* section */
-      (
-        'Compliance Over Time', /* name */
+    SELECT 'true' as [@json:Array],
         (
-          SELECT
-            'line' as [type],
+          SELECT 'true' as [@json:Array], *
+          FROM (VALUES
+            ('## Section 1'),
+            (' * List Item 1'),
+            (' * List Item 2')
+          ) as md ([markdown])
+          FOR XML PATH ('markdown'), type
+        ),
+        (
+          SELECT 'true' as [@json:Array], *
+          FROM (VALUES
             (
-              SELECT
-                (
-                  SELECT [labels]
-                  FROM (VALUES ('January'), ('February'), ('March'), ('April'), ('May'), ('June'), ('July')) as l ([labels])
-                  FOR XML PATH (''), type
-                ),
-                (
-                  SELECT
-                    'true' as [@json:Array],
-					          'My First dataset' as [label],
-                    'red' as [backgroundColor],
-                    'red' as [borderColor],
-                    (
-                      SELECT [data/@Integer]
-                      FROM (VALUES (10), (60), (15), (5), (30), (38), (60)) as d ([data/@Integer])
-                      FOR XML PATH (''), type
-                    ),
-                    'false' as [fill/@Boolean]
-                  FOR XML PATH ('datasets'), type
-                ),
-                (
-                  SELECT
-                    'true' as [@json:Array],
-                    'My Second dataset' as [label],
-                    'blue' as [backgroundColor],
-                    'blue' as [borderColor],
-                    (
-                      SELECT [data/@Integer]
-                      FROM (VALUES (5), (12), (90), (65), (20), (56), (12)) as d ([data/@Integer])
-                      FOR XML PATH (''), type
-                    ),
-                    'false' as [fill/@Boolean]
-                  FOR XML PATH ('datasets'), type
-                )
-              FOR XML PATH ('data'), type
-            ),
-            (
-              SELECT
-                'true' as [responsive/@Boolean],
-                (
-                  SELECT
-                    'true' as [display/@Boolean],
-                    'Chart.js Line Chart' as [text]
-                  FOR XML PATH ('title'), type
-                ),
-                (
-                  SELECT
-                    'index' as [mode],
-                    'false' as [intersect/@Boolean]
-                  FOR XML PATH ('tooltips'), type
-                ),
-                (
-                  SELECT
-                    'nearest' as [mode],
-                    'true' as [intersect/@Boolean]
-                  FOR XML PATH ('hover'), type
-                ),
-                (
-                  SELECT
-                    (
-                      SELECT 'true' as [@json:Array], [display/@Boolean], [scaleLabel]
-                      FROM (VALUES
-                        (
-                          'true',
-                           (
-                             SELECT
-                               'true' as [display/@Boolean],
-                               'Month' as [labelString]
-                             FOR XML PATH (''), type
-                           )
-                        )
-                      ) as l ([display/@Boolean], [scaleLabel])
-                      FOR XML PATH ('xAxes'), type
-                    ),
-                    (
-                      SELECT 'true' as [@json:Array], [display/@Boolean], [scaleLabel]
-                      FROM (VALUES
-                        (
-                          'true',
-                           (
-                             SELECT
-                               'true' as [display/@Boolean],
-                               'Value' as [labelString]
-                             FOR XML PATH (''), type
-                           )
-                        )
-                      ) as l ([display/@Boolean], [scaleLabel])
-                      FOR XML PATH ('yAxes'), type
-                    )
-                  FOR XML PATH ('scales'), type
-                )
-              FOR XML PATH ('options'), type
+              (
+                SELECT
+                  'line-chart' as [type],
+                  (
+                    SELECT
+                      'line' as [type],
+                      (
+                        SELECT
+                          (
+                            SELECT [labels]
+                            FROM (VALUES ('January'), ('February'), ('March'), ('April'), ('May'), ('June'), ('July')) as l ([labels])
+                            FOR XML PATH (''), type
+                          ),
+                          (
+                            SELECT
+                              'true' as [@json:Array],
+                                        'My First dataset' as [label],
+                              'red' as [backgroundColor],
+                              'red' as [borderColor],
+                              (
+                                SELECT [data/@Integer]
+                                FROM (VALUES (10), (60), (15), (5), (30), (38), (60)) as d ([data/@Integer])
+                                FOR XML PATH (''), type
+                              ),
+                              'false' as [fill/@Boolean]
+                            FOR XML PATH ('datasets'), type
+                          ),
+                          (
+                            SELECT
+                              'true' as [@json:Array],
+                              'My Second dataset' as [label],
+                              'blue' as [backgroundColor],
+                              'blue' as [borderColor],
+                              (
+                                SELECT [data/@Integer]
+                                FROM (VALUES (5), (12), (90), (65), (20), (56), (12)) as d ([data/@Integer])
+                                FOR XML PATH (''), type
+                              ),
+                              'false' as [fill/@Boolean]
+                            FOR XML PATH ('datasets'), type
+                          )
+                        FOR XML PATH ('data'), type
+                      ),
+                      (
+                        SELECT
+                          'true' as [responsive/@Boolean],
+                          (
+                            SELECT
+                              'true' as [display/@Boolean],
+                              'Chart.js Line Chart' as [text]
+                            FOR XML PATH ('title'), type
+                          ),
+                          (
+                            SELECT
+                              'index' as [mode],
+                              'false' as [intersect/@Boolean]
+                            FOR XML PATH ('tooltips'), type
+                          ),
+                          (
+                            SELECT
+                              'nearest' as [mode],
+                              'true' as [intersect/@Boolean]
+                            FOR XML PATH ('hover'), type
+                          ),
+                          (
+                            SELECT
+                              (
+                                SELECT 'true' as [@json:Array], [display/@Boolean], [scaleLabel]
+                                FROM (VALUES
+                                  (
+                                    'true',
+                                     (
+                                       SELECT
+                                         'true' as [display/@Boolean],
+                                         'Month' as [labelString]
+                                       FOR XML PATH (''), type
+                                     )
+                                  )
+                                ) as l ([display/@Boolean], [scaleLabel])
+                                FOR XML PATH ('xAxes'), type
+                              ),
+                              (
+                                SELECT 'true' as [@json:Array], [display/@Boolean], [scaleLabel]
+                                FROM (VALUES
+                                  (
+                                    'true',
+                                     (
+                                       SELECT
+                                         'true' as [display/@Boolean],
+                                         'Value' as [labelString]
+                                       FOR XML PATH (''), type
+                                     )
+                                  )
+                                ) as l ([display/@Boolean], [scaleLabel])
+                                FOR XML PATH ('yAxes'), type
+                              )
+                            FOR XML PATH ('scales'), type
+                          )
+                        FOR XML PATH ('options'), type
+                      )
+                    FOR XML PATH (''), type
+                  ) as [config]
+                FOR XML PATH (''), type
+              )
             )
-          FOR XML PATH (''), type
-        ) /* chart */
-      )
-    ) as sections ([name], [chart])
+          ) as f ([figures])
+          FOR XML PATH ('figures'), type
+        )
     FOR XML PATH ('sections'), type
   ),
   (
-    SELECT
-      '© 2020 Health Catalyst. All Rights Reserved.' as [text]
+    SELECT 
+        (
+          SELECT 'true' as [@json:Array], *
+          FROM (VALUES
+            ('© 2020 Health Catalyst. All Rights Reserved.')
+          ) as md ([markdown])
+          FOR XML PATH ('markdown'), type
+        )
     FOR XML PATH ('footer'), type
   )
 FOR XML PATH (''), ROOT ('root')
